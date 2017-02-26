980878c76437623b5064976c226820059041241a
Russell Van Dam <revd3@gatech.edu>
2017-02-26 10:16:56 -0800

    Used code reformatter

--- bitcoin_forum_scraper.py
+++ bitcoin_forum_scraper.py
@@ -4,27 +4,32 @@ import re
 from hashlib import sha256
 import traceback
 
-#takes in a string and returns all potential bitcoin addresses in a list
+
+# takes in a string and returns all potential bitcoin addresses in a list
 def find_bitcoin_addr(text):
-    #Below regular expression should do the trick. Modified from the Bitcoin Transaction Analysis Graph Paper. 
-    #(https://arxiv.org/pdf/1502.01657.pdf)
-    #We need to support multisig bitcoin addresses that have a 3 in front of them. https://en.bitcoin.it/wiki/Address
+    # Below regular expression should do the trick. Modified from the Bitcoin Transaction Analysis Graph Paper.
+    # (https://arxiv.org/pdf/1502.01657.pdf)
+    # We need to support multisig bitcoin addresses that have a 3 in front of them. https://en.bitcoin.it/wiki/Address
     match = re.findall(b"[13].[a-zA-z0-9]{26,33}", text)
 
     if match:
         return match
-    else: 
+    else:
         return False
 
+
 digits58 = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
- 
+
+
 def decode_base58(bc, length):
-    print( bc)
+    print(bc)
     n = 0
     for char in bc:
         n = n * 58 + digits58.index(str(char))
         # n = n * 58 + 10
     return n.to_bytes(length, 'big')
+
+
 def check_bc(bc):
     bcbytes = decode_base58(bc, 25)
     return bcbytes[-4:] == sha256(sha256(bcbytes[:-4]).digest()).digest()[:4]
@@ -33,12 +38,12 @@ def check_bc(bc):
 class BitcoinSpider(scrapy.Spider):
     name = "bitcoin_forum"
     start_urls = [
-            # 'http://bitcointalk.org',
-            #'http://bitcointalk.org/index.php?topic=20333.0'
-            'http://bitcointalk.org/index.php'
-            # 'file:///home/brian/network_security/dev/tuturial/bitcointalk-0.html'
+        # 'http://bitcointalk.org',
+        # 'http://bitcointalk.org/index.php?topic=20333.0'
+        'http://bitcointalk.org/index.php'
+        # 'file:///home/brian/network_security/dev/tuturial/bitcointalk-0.html'
 
-        ]
+    ]
     allowed_domains = ["bitcointalk.org"]
     page = 0
 
@@ -51,14 +56,14 @@ class BitcoinSpider(scrapy.Spider):
         #     f.write(response.body)
         # self.log('Saved file %s' % filename)
         # self.page = self.page +1
-        
+
         potential_matches = find_bitcoin_addr(response.body)
         filename = 'found_addresses.txt'
-        print ("LENGTH OF POTENTIAL MATCHES: "+str(len(potential_matches)))
-        
-        with open(filename,'wb') as f:
+        print ("LENGTH OF POTENTIAL MATCHES: " + str(len(potential_matches)))
+
+        with open(filename, 'wb') as f:
             for item in potential_matches:
-                f.write(item+b'\n')
+                f.write(item + b'\n')
                 addr_found = False
                 try:
                     addr_found = check_bc(item)
@@ -71,8 +76,3 @@ class BitcoinSpider(scrapy.Spider):
                     print(traceback.print_exc())
                 if addr_found:
                     f.write(item)
-            
-
-
-
-
