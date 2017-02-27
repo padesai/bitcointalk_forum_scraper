09dea3676c4e4968279da69e9b312753f14d2706
Russell Van Dam <revd3@gatech.edu>
2017-02-27 12:57:12 -0800

    Fixed Navigation issues and commented a little

--- bitcoin_forum_scraper.py
+++ bitcoin_forum_scraper.py
@@ -4,6 +4,10 @@ import re
 from hashlib import sha256
 import traceback
 
+# Run Program with 'scrapy runspider bitcoin_forum_scraper.py -o out.json -s DOWNLOAD_DELAY=1 --nolog'
+
+digits58 = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+
 
 # takes in a string and returns all potential bitcoin addresses in a list
 def find_bitcoin_addr(text):
@@ -18,11 +22,8 @@ def find_bitcoin_addr(text):
         return []
 
 
-digits58 = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
-
-
 def decode_base58(bc, length):
-    print(bc)
+    #print(bc)
     n = 0
     for char in bc:
         n = n * 58 + digits58.index(str(char))
@@ -31,76 +32,91 @@ def decode_base58(bc, length):
 
 
 def check_bc(bc):
-
     bcbytes = decode_base58(bc, 25)
     return bcbytes[-4:] == sha256(sha256(bcbytes[:-4]).digest()).digest()[:4]
 
+
 def test_bitcoin_validity_func():
     print("Checking validity of some test bitcoin addresses...")
-    print("Should be True, actually is "+str(check_bc('18vaZ4K62WiL6W2Qoj9AE1cerfCHRaUW4x')))
+    print("Should be True, actually is " + str(check_bc('18vaZ4K62WiL6W2Qoj9AE1cerfCHRaUW4x')))
 
 
 test_bitcoin_validity_func()
+
 
 class BitcoinSpider(scrapy.Spider):
     name = "bitcoin_forum"
     start_urls = [
         # 'http://bitcointalk.org',
-        'https://bitcointalk.org/index.php?topic=423995.0'
+        # 'https://bitcointalk.org/index.php?topic=423995.0'
         # 'https://bitcointalk.org/index.php?topic=20333.0',
         # 'https://bitcointalk.org/index.php?topic=1791378.0'
-       ]
+        'https://bitcointalk.org/index.php?board=1.0'
+    ]
     allowed_domains = ["bitcointalk.org"]
     page = 0
 
-    #     for url in start_urls:
-    #         yield scrapy.Request(url=url, callback=self.parse_url)
-
     def parse(self, response):
-        next_page = response.css('#bodyarea div a+ a ::attr(href)').extract_first()
-        if next_page is not None:
-            # print(next_page)
-            #if (next_page[-19:] == ";prev_next=next#new"):
-            ##    page = next_page[:-19]
-            #else:
-            page = next_page
-            print(page)
-            yield scrapy.Request(page, callback=self.parse)
-            yield scrapy.Request(page, callback=self.parse_thread)
+        # Used to find Threads on a Board Page. Will navigate to next Board Page
+        PRINT_LOGS = True
+
+        # Navigate to each Thread on the Board Page
+        for href in response.css('.leftimg+ td span a ::attr(href)').extract():
+            if PRINT_LOGS: print("On Board {}, yielding thread {}".format(response.url, href))
+            yield scrapy.Request(href, callback=self.parse_thread)
+
+        # Navigate to the next Board Page
+        next_page = response.css('.prevnext .navPages ::attr(href)').extract()
+        if len(next_page) > 0:
+            if PRINT_LOGS: print("On Board {}, yielding Board {}".format(response.url, next_page[-1]))
+            yield scrapy.Request(next_page[-1], callback=self.parse)
 
     def parse_thread(self, response):
-        potential_matches = find_bitcoin_addr(response.body)
-        valid_addresses = []
-        unique_matches = set(potential_matches)
-        for item in unique_matches:
+        # Used to search for Bitcoins on a Thread Page. Will navigate to next Thread Page
+        PRINT_LOGS = True
+
+        # Outputs to the output file for each Thread Page with valid Bitcoins
+        valid_addresses = collect_bitcoins(response.body)
+        if len(valid_addresses) > 0:
+            print("parse_thread: found {} on {}.".format(valid_addresses, response.url))
+            yield {"url": response.url, "bitcoin_addresses": valid_addresses}
+
+        # Navigate to the next page in the Thread
+        next_page = response.css('.prevnext .navPages ::attr(href)').extract()
+        if len(next_page) > 0:
+            if PRINT_LOGS: print("On P {}, yielding P {}".format(response.url, next_page[-1]))
+            yield scrapy.Request(next_page[-1], callback=self.parse_thread)
+
+            # '.poster_info b a' is the css selector for a url to the poster's profile
+
+
+def collect_bitcoins(body):
+    # Given an HTML page, returns a list of all unique, valid bitcoins on that page
+    potential_matches = find_bitcoin_addr(body)
+    valid_addresses = []
+    unique_matches = set(potential_matches)
+
+    # Try to validate each regex matched address
+    for item in unique_matches:
+        addr_found = False
+        str_item = item.decode("utf-8")
+        try:
+            addr_found = check_bc(str_item)
+
+        except AttributeError:
+            print("Please run with Python3!")
+            exit()
+
+        except ValueError:
             addr_found = False
-            str_item = item.decode("utf-8")
-            try:
-                addr_found = check_bc(str_item)
-            except AttributeError:
-                print("Please run with Python3!")
-                exit()
-            except ValueError:
 
-                addr_found = False
-            except TypeError:
-                print(traceback.print_exc())
-            except OverflowError:
-                print(traceback.print_exc())
-            if addr_found: 
-                valid_addresses.append(str_item)
+        except TypeError:
+            print(traceback.print_exc())
 
-        if len(valid_addresses)>0:
-            yield {"url": response.url, 
-                "bitcoin_addresses": list(valid_addresses)}
+        except OverflowError:
+            print(traceback.print_exc())
 
-        #.poster_info b a' is the css selector for a url to the poster's profile
+        if addr_found:
+            valid_addresses.append(str_item)
 
-        next_page = response.css('div+ table .middletext > .navPages ::attr(href)').extract()
-
-        for page in next_page:
-            print(page)
-            yield scrapy.Request(page, callback=self.parse_thread)
-
-    
-
+    return valid_addresses
