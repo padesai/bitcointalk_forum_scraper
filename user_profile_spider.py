0b4e52850da395aabb672efeeaa87911fa83ab12
brian <brian_edmonds@gatech.edu>
2017-03-05 14:59:22 -0500

    correctly spiders a single thread page for user profiles and then gets those user profile bitcoin address if they are listed

--- user_profile_spider.py
+++ user_profile_spider.py
@@ -9,17 +9,22 @@ class UserProfileSpider(scrapy.Spider):
     name = "user_profile_spider"
     start_urls = [
         # "https://bitcointalk.org/index.php?action=profile;u=10197",
-        "https://bitcointalk.org/index.php?action=profile;u=648651"
+        "https://bitcointalk.org/index.php?topic=1074337.400"
     ]
     allowed_domains = ["bitcointalk.org"]
     page = 0
 
-    def parse(self, response):
+
+
+    def parse_user_profile(self, response):
         # Used to find Threads on a Board Page. Will navigate to next Board Page
         bitcoins = bitcoin_helper.collect_bitcoins(response.body)
         # print("LENGTH OF LIST: "+str(len(bitcoins)))
         if len(bitcoins)>0:
-            
             user_id = response.css('.windowbg tr:nth-child(1) td:nth-child(2)::text').extract_first()
-            yield {"user_id": user_id, "bitcoin_addr":bitcoins[0]}
+            yield {"user_id": user_id, "Profile URL": response.url, "bitcoin_addresses": bitcoins}
 
+    def parse(self, response):
+        list_users = response.css('.poster_info b a::attr(href)').extract()
+        for href in list_users:
+            yield scrapy.Request(href, callback=self.parse_user_profile)
