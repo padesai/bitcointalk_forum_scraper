a5f52c983c8e6136865b91a6623efb6d804d4df9
brian <brian_edmonds@gatech.edu>
2017-03-08 17:24:56 -0500

    made the database population better

--- update_database.py
+++ update_database.py
@@ -14,19 +14,21 @@ def readConfigFile(config_file):
 	values.append(password)
 	return values
 
+def get_db_id_of_user(parsed_username)
+	#get the foreign key for the user that is an owner of the bitcoin address
+	sql = "Select id from user_profiles where username = '%s'"%(parsed_username)
+    cursor.execute(sql)
+    user_id = cursor.fetchone()[0]
+
 #get the configuration information for the mysql database
 configValues = readConfigFile('mysqlconfig.txt')
 username = configValues[0]
 user_pass = configValues[1]
 
-
-
 #connect to the mysql database
 cnx = mysql.connector.connect(user=username, password=user_pass,
                               host='127.0.0.1',
                               database='bitcoin_forum_scraping')
-
-
 
 #open the JSON file
 with open('user_profile.json') as data_file:    
@@ -34,22 +36,28 @@ with open('user_profile.json') as data_file:
     for line_item in data:
     	parsed_username = line_item['user_id']
     	parsed_url = line_item['Profile URL']
-      	sql = "INSERT INTO user_profiles (username, profile_url) VALUES ('%s','%s')"%(parsed_username, parsed_url)
+    	#SQL query that will add a username and profile url for the parsed data
+      	sql = "INSERT IGNORE INTO user_profiles (username, profile_url) VALUES ('%s','%s')"%(parsed_username, parsed_url)
       	cursor = cnx.cursor(buffered=True)
-      	cursor.execute(sql)	
-      	cnx.commit()
-      	#Get the primary key of the last inserted user ID
-      	sql = "SELECT LAST_INSERT_ID();"
-      	cursor.execute(sql)
-      	user_id = cursor.fetchone()[0]
+      	try:
+      		cursor.execute(sql)	
+      		cnx.commit()
+      	except mysql.connector.errors.IntegrityError:
+      		print "Tried to insert a duplicate username: '%s'"%(parsed_username)
 
       	#Get the bitcoin addresses found for the scraped data
       	bitcoin_addresses = line_item['bitcoin_addresses']
+      	user_id = get_db_id_of_user(parsed_username)
+      	
       	for addr in bitcoin_addresses:
       		sql = "INSERT INTO user_bitcoins (user_profile_id,bitcoin_address) VALUES ('%s','%s')"%(user_id,addr)
       		cursor.execute(sql)
       		cnx.commit()
 
+# with open('comments.json') as comments_file:
+# 	comments_data = json.load(comments_file)
+# 	for line_item in comments_data:
+# 		parsed_user
 
 cnx.close()
 
